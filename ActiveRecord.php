171d8c505ceb1989e72842e411c55f72a5e132e4
sanchezzzhak <sanchezzzhak@ya.ru>
2016-07-28 19:01:21 +0300

    AR v1

---
+++ ActiveRecord.php
@@ -0,0 +1,129 @@
+<?php
+namespace kak\clickhouse;
+use yii\base\InvalidConfigException;
+use yii\db\BaseActiveRecord;
+use Yii;
+use yii\db\TableSchema;
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+class ActiveRecord extends BaseActiveRecord
+{
+
+    /**
+     * Declares the name of the database table associated with this AR class.
+     * By default this method returns the class name as the table name by calling [[Inflector::camel2id()]]
+     * with prefix [[Connection::tablePrefix]]. For example if [[Connection::tablePrefix]] is 'tbl_',
+     * 'Customer' becomes 'tbl_customer', and 'OrderItem' becomes 'tbl_order_item'. You may override this method
+     * if the table is not named after this convention.
+     * @return string the table name
+     */
+    public static function tableName()
+    {
+        return '{{%' . Inflector::camel2id(StringHelper::basename(get_called_class()), '_') . '}}';
+    }
+
+    /**
+     * Returns the schema information of the DB table associated with this AR class.
+     * @return TableSchema the schema information of the DB table associated with this AR class.
+     * @throws InvalidConfigException if the table for the AR class does not exist.
+     */
+    public static function getTableSchema()
+    {
+        $tableSchema = static::getDb()
+            ->getSchema()
+            ->getTableSchema(static::tableName());
+
+        if ($tableSchema === null) {
+            throw new InvalidConfigException('The table does not exist: ' . static::tableName());
+        }
+
+        return $tableSchema;
+    }
+
+    /**
+     * Returns the list of all attribute names of the model.
+     * The default implementation will return all column names of the table associated with this AR class.
+     * @return array list of attribute names.
+     */
+    public function attributes()
+    {
+        return array_keys(static::getTableSchema()->columns);
+    }
+
+    /**
+     * Returns the connection used by this AR class.
+     * @return mixed|Connection the database connection used by this AR class.
+     */
+    public static function getDb()
+    {
+        return Yii::$app->get('clickhouse');
+    }
+
+    /**
+     * @inheritdoc
+     * @return ActiveQuery the newly created [[ActiveQuery]] instance.
+     */
+    public static function find()
+    {
+        return Yii::createObject(ActiveQuery::className(), [get_called_class()]);
+    }
+
+    /**
+     * Returns the primary key **name(s)** for this AR class.
+     *
+     * Note that an array should be returned even when the record only has a single primary key.
+     *
+     * For the primary key **value** see [[getPrimaryKey()]] instead.
+     *
+     * @return string[] the primary key name(s) for this AR class.
+     */
+    public static function primaryKey()
+    {
+        // TODO: Implement primaryKey() method.
+        return null;
+    }
+
+    /**
+     * Inserts the record into the database using the attribute values of this record.
+     *
+     * Usage example:
+     *
+     * ```php
+     * $customer = new Customer;
+     * $customer->name = $name;
+     * $customer->email = $email;
+     * $customer->insert();
+     * ```
+     *
+     * @param boolean $runValidation whether to perform validation (calling [[validate()]])
+     * before saving the record. Defaults to `true`. If the validation fails, the record
+     * will not be saved to the database and this method will return `false`.
+     * @param array $attributes list of attributes that need to be saved. Defaults to null,
+     * meaning all attributes that are loaded from DB will be saved.
+     * @return boolean whether the attributes are valid and the record is inserted successfully.
+     */
+    public function insert($runValidation = true, $attributes = null)
+    {
+        if ($runValidation && !$this->validate($attributes)) {
+            Yii::info('Model not inserted due to validation error.', __METHOD__);
+            return false;
+        }
+        if (!$this->beforeSave(true)) {
+            return false;
+        }
+
+        $values = $this->getDirtyAttributes($attributes);
+        if ((static::getDb()->getSchema()->insert(static::tableName(), $values)) === false) {
+            return false;
+        }
+
+        $changedAttributes = array_fill_keys(array_keys($values), null);
+        $this->setOldAttributes($values);
+        $this->afterSave(true, $changedAttributes);
+        return true;
+
+    }
+
+
+}
