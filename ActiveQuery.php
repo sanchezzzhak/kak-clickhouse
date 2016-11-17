70f08cdb2dcb82048e6e609dbe9fa028762070f4
sanchezzzhak <sanchezzzhak@ya.ru>
2016-11-17 15:58:36 +0300

    remove not uses class

--- ActiveQuery.php
+++ ActiveQuery.php
@@ -1,12 +1,6 @@
 <?php
 namespace kak\clickhouse;
-
-
-use yii\base\Component;
 use yii\db\ActiveQueryInterface;
-use yii\db\ActiveRecordInterface;
-use yii\db\Connection;
-use yii\db\QueryInterface;
 
 class ActiveQuery extends \yii\db\ActiveQuery implements ActiveQueryInterface
 {
