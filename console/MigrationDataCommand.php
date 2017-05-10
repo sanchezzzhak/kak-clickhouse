29d9f2518b397dd4fbaecdd74366d677ca0a616c
sanchezzzhak <sanchezzzhak@yandex.ru>
2017-05-10 10:29:22 +0300

    prepare data export

--- console/MigrationDataCommand.php
+++ console/MigrationDataCommand.php
@@ -6,6 +6,8 @@
  * Time: 12:25
  */
 namespace kak\clickhouse\console;
+use kak\clickhouse\ColumnSchema;
+use kak\clickhouse\TableSchema;
 use yii\base\Object;
 use Yii;
 
@@ -17,6 +19,10 @@ use yii\helpers\FileHelper;
  */
 class MigrationDataCommand extends Object
 {
+    const FORMAT_CSV           = 'CSV';
+    const FORMAT_JSON_EACH_ROW = 'JSONEachRow';
+
+
     /** @var string source table name */
     public $sourceTable;
     /** @var \yii\db\Connection */
@@ -29,6 +35,7 @@ class MigrationDataCommand extends Object
     public $storeDb;
     /** @var int size data and step export data */
     public $batchSize = 10000;
+    public $format = self::FORMAT_CSV;
 
 
     /** @var array  'store_column' => 'source_column' */
@@ -44,23 +51,48 @@ class MigrationDataCommand extends Object
         }
     }
 
-
-    private function arrayToValueStr($row)
+    /**
+     * @param $row
+     * @return string
+     */
+    private function prepareExportData($row)
     {
         $out = [];
-
-        foreach ($this->mapSchemaData as $key => $map){
+        foreach ($this->mapSchemaData as $key => $item){
             $val = '';
-            if($map instanceof \Closure){
-                $val = call_user_func_array($map,$row);
+            if($item instanceof \Closure){
+                $val = call_user_func($item, $row);
+                $val = $this->castTypeValue($key, $val);
             }
-            $out[$key] = $row;
+            elseif(is_string($item) && isset($row[$item])){
+                $val = $this->castTypeValue($key,$row[$item]);
+            }else if(isset($this->_schema->columns[$key])) {
+                $val = $this->castTypeValue($key,$item);
+            }
+
+            $out[$key] = $val;
         }
+
+        if ($this->format == self::FORMAT_JSON_EACH_ROW) {
+            return json_encode($out);
+        }
+
+        //FORMAT_CSV
         return implode(',',$out);
     }
 
-    private $_schema;
+    private function castTypeValue($key,$val)
+    {
+        $column = isset($this->_schema->columns[$key]) ? $this->_schema->columns[$key] : null;
+        if($column!==null) {
+            $val = $this->sourceDb->quoteValue($column->phpTypecast($val));
+        }
+        return $val;
+    }
 
+
+    /** @var  TableSchema */
+    private $_schema;
 
     public function run()
     {
@@ -71,9 +103,9 @@ class MigrationDataCommand extends Object
         }
 
         $this->_schema = $this->storeDb->getTableSchema($this->storeTable,true);
+        //var_dump($this->_schema->columns);
         //var_dump($this->_schema);
 
-        exit;
         $countTotal = (new \yii\db\Query())->from($this->sourceTable)->limit(1)->count('id',$this->sourceDb);
         echo "total count rows source table {$countTotal}\n";
         $partCount = ceil($countTotal/ $this->batchSize);
@@ -92,11 +124,11 @@ class MigrationDataCommand extends Object
             $path = $dir . '/part' . $i. '.data';
             $lines = '';
             foreach ($rows as $row){
-                $lines.="" . $this->arrayToValueStr($row) . "\n";
+                $lines.= $this->prepareExportData($row) . "\n";
             }
-            var_dump('111',$lines);
+            var_dump($lines);
 
-            exit;
+
             $files[] = $path;
             file_put_contents($path,$lines);
         }
