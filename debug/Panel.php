78a1756aaae03a8dd0dad7e0a247e9f7fcfe160b
sanchezzzhak <sanchezzzhak@ya.ru>
2017-10-09 15:30:12 +0300

    fix db-panel icon style position

--- debug/Panel.php
+++ debug/Panel.php
@@ -10,7 +10,7 @@ class Panel extends \yii\debug\panels\DbPanel
      */
     public $db = 'clickhouse';
 
-    public $logo = '<svg xmlns="http://www.w3.org/2000/svg" width="33" height="30" viewBox="0 0 9 8" style="position: absolute;bottom: 4px;"><path fill="red" d="M0,7 h1 v1 h-1 z"></path><path fill="#fc0" d="M0,0 h1 v7 h-1 z"></path><path fill="#fc0" d="M2,0 h1 v8 h-1 z"></path><path fill="#fc0" d="M4,0 h1 v8 h-1 z"></path><path fill="#fc0" d="M6,0 h1 v8 h-1 z"></path><path fill="#fc0" d="M8,3.25 h1 v1.5 h-1 z"></path></svg><i style="display: inline-block;width: 35px"></i>';
+    public $logo = '<svg xmlns="http://www.w3.org/2000/svg" width="33" height="30" viewBox="0 0 9 8" style="position: absolute; z-index: 1"><path fill="red" d="M0,7 h1 v1 h-1 z"></path><path fill="#fc0" d="M0,0 h1 v7 h-1 z"></path><path fill="#fc0" d="M2,0 h1 v8 h-1 z"></path><path fill="#fc0" d="M4,0 h1 v8 h-1 z"></path><path fill="#fc0" d="M6,0 h1 v8 h-1 z"></path><path fill="#fc0" d="M8,3.25 h1 v1.5 h-1 z"></path></svg><i style="display: inline-block;width: 35px"></i>';
 
     /**
      * @inheritdoc
