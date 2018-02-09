353e1e8eb4b64babe2124f0aa712828a4dfad7d1
sanchezzzhak <sanchezzzhak@ya.ru>
2018-02-09 17:24:41 +0300

    export  add input console mode 1 export all data after import data 2 export part import part etc.

--- console/MigrationDataCommand.php
+++ console/MigrationDataCommand.php
@@ -10,9 +10,10 @@ namespace kak\clickhouse\console;
 
 use kak\clickhouse\ColumnSchema;
 use kak\clickhouse\TableSchema;
-use yii\base\Object;
+use yii\base\BaseObject;
 use Yii;
 
+use yii\helpers\Console;
 use yii\helpers\FileHelper;
 use yii\helpers\VarDumper;
 
@@ -20,7 +21,7 @@ use yii\helpers\VarDumper;
  * Class MigrationDataCommand
  * @package kak\clickhouse\console
  */
-class MigrationDataCommand extends Object
+class MigrationDataCommand extends BaseObject
 {
     const FORMAT_CSV = 'CSV';
     const FORMAT_JSON_EACH_ROW = 'JSONEachRow';
@@ -187,83 +188,131 @@ class MigrationDataCommand extends Object
         $this->setFiles($files);
     }
 
-    /**
-     * save source-table data to files
-     * @return array
-     */
-    public function saveDump()
+
+    private $_countTotal;
+    private $_sourceDir;
+    private $_partCount;
+    private $_startPage;
+    private $_mode;
+
+
+    private function checkFilesExist()
     {
-        $dir = Yii::getAlias('@app/runtime/clickhouse') . "/" . $this->storeTable;
-        if (!file_exists($dir)) {
-            echo "create dir {$dir}\n";
-            FileHelper::createDirectory($dir);
-        }
-
-        $this->checkTableSchema();
-        $countTotal = $this->getTotalRows();
-
-        $useFilterSourceRow = $this->filterSourceRow !== null && $this->filterSourceRow instanceof \Closure;
-
-        echo "total count rows source table {$countTotal}\n";
-        $partCount = ceil($countTotal / $this->batchSize);
-        echo "part data files count {$partCount}\n";
-        echo "save files dir: {$dir} \n";
-        echo "parts:\n";
-
-        $this->_files = [];
-        for ($i = 0; $i < $partCount; $i++) {
-            $timer = microtime(true);
-            $file = 'part' . $i . '.data';
-            $path = $dir . '/' . $file;
-
-            $offset = ($i) * $this->batchSize;
-            $rows = $this->getRows($offset);
-            $lines = '';
-            foreach ($rows as $row) {
-                if ($useFilterSourceRow && !call_user_func($this->filterSourceRow, $row)) {
-                    continue;
+        if(file_exists($this->_sourceDir)){
+            $files = FileHelper::findFiles( $this->_sourceDir);
+            if(count($files)){
+                if (Console::confirm("remove all files dump?")) {
+                    FileHelper::removeDirectory($this->_sourceDir);
+                    $this->checkSourceDir();
+                    return true;
                 }
-                $lines .= $this->prepareExportData($row) . "\n";
             }
-            $this->_files[] = $path;
-            file_put_contents($path, $lines);
-
-            $timer = sprintf('%0.3f', microtime(true) - $timer);
-            echo " >>> " . $file . " time {$timer} \n";
         }
-
-        return $this->_files;
     }
 
-    /**
-     * import data clickhouse
-     */
-    public function importDump()
+    private function checkSourceDir()
     {
-        $files = $this->_files;
-        if (count($files)) {
-            $keys = [];
-
-            foreach ($this->mapData as $key => $item) {
-                $keys[] = $key;
-            }
-            echo "insert files \n";
-            foreach ($files as $file) {
-                $timer = microtime(true);
-                $responces = $this->storeDb->createCommand()->batchInsertFiles($this->storeTable, $keys, [$file], $this->format);
-                $timer = sprintf('%0.3f', microtime(true) - $timer);
-                $partName = pathinfo($file, PATHINFO_BASENAME);
-                if (!$responces[0]->getIsOk()) {
-                    echo " !!! " . $partName . " time {$timer} error: " . $responces[0]->getContent();
-                    continue;
-                }
-                echo " <<< " . $partName . " time {$timer}\n";
-            }
+        if (!file_exists($this->_sourceDir)) {
+            echo "create dir {$this->_sourceDir}\n";
+            FileHelper::createDirectory($this->_sourceDir);
         }
+    }
+
+    private function runConfiguration()
+    {
+        $this->_sourceDir = Yii::getAlias('@app/runtime/clickhouse') . "/" . $this->storeTable;
+
+        $this->checkSourceDir();
+        $this->checkTableSchema();
+        $this->checkFilesExist();
+
+
+        $this->_countTotal = $this->getTotalRows();
+        echo "total count rows source table {$this->_countTotal}\n";
+        $this->_partCount = ceil($this->_countTotal / $this->batchSize);
+        echo "part data files count {$this->_partCount}\n";
+        echo "save files dir: {$this->_sourceDir} \n";
+        echo "parts:\n";
+
+        if(empty($this->_mode)){
+            $this->_mode = Console::input("select export mode:\n1 all import after export\n2 pages import page after export page\n");
+        }
+
+        $this->_startPage = Console::input('select page: 0 ... ' . $this->_partCount . "\n");
+
+
+
+    }
+
+    private function runProcess()
+    {
+        switch($this->_mode){
+            default:
+            case '1':
+                for ($i = $this->_startPage; $i < $this->_partCount; $i++) {
+                    $this->saveSource($i);
+                }
+                foreach ($this->_files as $i => $file){
+                    $this->sendSource($i);
+                }
+                break;
+            case '2':
+                for ($i = $this->_startPage; $i < $this->_partCount; $i++) {
+                    $this->saveSource($i);
+                    $this->sendSource($i);
+                }
+                break;
+        }
+
 
         echo "done\n";
     }
 
+
+    private function saveSource($i)
+    {
+        $useFilterSourceRow = $this->filterSourceRow !== null && $this->filterSourceRow instanceof \Closure;
+
+        $timer = microtime(true);
+        $file = 'part' . $i . '.data';
+        $path = $this->_sourceDir . '/' . $file;
+
+        $offset = ($i) * $this->batchSize;
+        $rows = $this->getRows($offset);
+        $lines = '';
+        foreach ($rows as $row) {
+            if ($useFilterSourceRow && !call_user_func($this->filterSourceRow, $row)) {
+                continue;
+            }
+            $lines .= $this->prepareExportData($row) . "\n";
+        }
+        $this->_files[$i] = $path;
+        file_put_contents($path, $lines);
+
+        $timer = sprintf('%0.3f', microtime(true) - $timer);
+        echo " >>> " . $file . " time {$timer} \n";
+
+    }
+    private function sendSource($i)
+    {
+        $file = $this->_files[$i];
+        $timer = microtime(true);
+        foreach ($this->mapData as $key => $item) {
+            $keys[] = $key;
+        }
+        $partName = pathinfo($file, PATHINFO_BASENAME);
+        $responces = $this->storeDb->createCommand()->batchInsertFiles($this->storeTable, $keys, [$file], $this->format);
+        $ok = $responces[0]->getIsOk();
+
+        $timer = sprintf('%0.3f', microtime(true) - $timer);
+        if (!$ok) {
+            echo " !!! " . $partName . " time {$timer} error: " . $responces[0]->getContent();
+        }else {
+            echo " <<< " . $partName . " time {$timer}\n";
+        }
+
+
+    }
 
     /**
      * 1 export data mysql
@@ -272,10 +321,9 @@ class MigrationDataCommand extends Object
      */
     public function run($insert = true)
     {
-        $this->saveDump();
-        if ($insert) {
-            $this->importDump();
-        }
+
+        $this->runConfiguration();
+        $this->runProcess();
     }
 
 }
