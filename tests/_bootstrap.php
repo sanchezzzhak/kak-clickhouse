2569f413cb686941ae41b8d001da8377760fbcb9
Tutik Alexsandr <sanchezzzhak@ya.ru>
2021-10-06 23:08:56 +0300

    feat(tests) fix tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,10 +4,9 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 
 $autoloadGitHub = dirname(__DIR__, 1) . '/vendor/autoload.php';
 $yiiGitHub = dirname(__DIR__, 1) . '/vendor/yiisoft/yii2/Yii.php';
-require_once $autoloadGitHub;
-require_once $yiiGitHub;
 
-use Yii;
+require_once $yiiGitHub;
+require_once $autoloadGitHub;
 
 Yii::setAlias('@tests', __DIR__);
 Yii::setAlias('@data', __DIR__ . DIRECTORY_SEPARATOR . '_data');
--- tests/_config/unit.php
+++ tests/_config/unit.php
@@ -1,4 +1,7 @@
 <?php
+use kak\clickhouse\Connection as ClickhouseConnection;
+
+
 return [
     'id' => 'test-clickhouse-console',
     'class' => 'yii\console\Application',
@@ -6,6 +9,16 @@ return [
     'runtimePath' => \Yii::getAlias('@tests/_output'),
     'bootstrap' => [],
     'components' => [
-        'clickhouse' => require ('clickhouse.php'),
+        'clickhouse' => array_merge([
+            'class' => ClickhouseConnection::class,
+            'dsn' => 'localhost',
+            'port' => 8123,
+            'database' => 'test_clickhouse',
+            'username' => '',
+            'password' => '',
+            'enableSchemaCache' => false,
+            'schemaCache' => 'cache',
+            'schemaCacheDuration' => 604800
+        ], is_file(__DIR__ . '/clickhouse.php') ? require __DIR__ . '/clickhouse.php' : []),
     ]
 ];
